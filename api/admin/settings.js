8bfedc67b045a14748a3cb605f1140595be1e1a9
YARNAV <arnavpagare200827@gmail.com>
2026-07-22 17:48:09 +0530

    Remove EmailJS env var checks from admin settings

--- api/admin/settings.js
+++ api/admin/settings.js
@@ -7,9 +7,6 @@ export default function handler(req, res) {
     ADMIN_PASSWORD: !!process.env.ADMIN_PASSWORD,
     ADMIN_JWT_SECRET: !!process.env.ADMIN_JWT_SECRET,
     ENCRYPTION_KEY: !!process.env.ENCRYPTION_KEY,
-    VITE_EMAILJS_SERVICE_ID: !!process.env.VITE_EMAILJS_SERVICE_ID,
-    VITE_EMAILJS_TEMPLATE_ID: !!process.env.VITE_EMAILJS_TEMPLATE_ID,
-    VITE_EMAILJS_PUBLIC_KEY: !!process.env.VITE_EMAILJS_PUBLIC_KEY,
     VITE_GA_ID: !!process.env.VITE_GA_ID,
     VITE_CLARITY_ID: !!process.env.VITE_CLARITY_ID,
   }
